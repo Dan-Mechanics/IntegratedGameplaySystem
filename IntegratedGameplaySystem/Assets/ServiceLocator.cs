18601f235667c58133694e286bc42eee0566aa36
Dan-Mechanics <github.tim@desmidt.org>
2025-05-26 21:15:41 +0200

    Create ServiceLocator.cs

---
+++ IntegratedGameplaySystem/Assets/ServiceLocator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace IntegratedGameplaySystem
+{
+    public static class ServiceLocator<T>
+    {
+        public static T instance;
+        public static T GetService
+        {
+            get
+            {
+                if (instance == null)
+                    Debug.LogError("No instance provided yet !!");
+
+                return instance;
+            }
+        }
+
+        public static void Provide(T service) 
+        {
+            instance = service;
+        }
+    }
+}
